db619f610aaf119c9c3c60c5a7cf7e427f0ba5d5
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-03-01 17:25:07 +0530

    JS for drawing snake on game board

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
 
+const gameBoard = document.getElementById("game-board");
 let lastRenderTime = 0;
 
 function main(currentTime) {
@@ -20,5 +21,5 @@ function update() {
 }
 
 function draw() {
-    drawSnake();
+    drawSnake(gameBoard);
 }
--- snake.js
+++ snake.js
@@ -1,9 +1,17 @@
 export const SNAKE_SPEED = 2; // how many times the snake moves in one second
+const snakeBody = [{ x: 11, y: 11 }]; // snake is represented from 21x21 grid in game-board
 
 export function update() {
     console.log("update snake");
 }
 
-export function draw() {
-    console.log("draw snake");
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div');
+        snakeElement.classList.add('snake');
+        snakeElement.style.gridRowStart = segment.x;
+        snakeElement.style.gridColumnStart = segment.y;
+
+        gameBoard.appendChild(snakeElement);
+    });
 }
