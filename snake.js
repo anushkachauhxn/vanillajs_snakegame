8ace06f7efcdb79170c1bb3315d8ab4c51f28e9e
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-03-01 17:49:07 +0530

    JS for updating snake's position

--- snake.js
+++ snake.js
@@ -1,16 +1,28 @@
 export const SNAKE_SPEED = 2; // how many times the snake moves in one second
-const snakeBody = [{ x: 11, y: 11 }]; // snake is represented from 21x21 grid in game-board
+const snakeBody = [
+    { x: 10, y: 11 },
+    { x: 11, y: 11 },
+    { x: 12, y: 11 },
+]; // snake body is represented from 21x21 grid in game-board
 
 export function update() {
-    console.log("update snake");
+    // iterate over snake elements starting from second last element (length - 2); 
+    // since last element (length - 1) is going to disappear after update
+    for (let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = { ...snakeBody[i] };
+    }
+
+    // update head according to key
+    snakeBody[0].x += 1;
+    snakeBody[0].y += 0;
 }
 
 export function draw(gameBoard) {
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div');
         snakeElement.classList.add('snake');
-        snakeElement.style.gridRowStart = segment.x;
-        snakeElement.style.gridColumnStart = segment.y;
+        snakeElement.style.gridColumnStart = segment.x;
+        snakeElement.style.gridRowStart = segment.y;
 
         gameBoard.appendChild(snakeElement);
     });
