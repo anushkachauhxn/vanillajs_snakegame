267e57064d6db58a1a820fbd625068b148f28e2a
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-03-05 10:23:18 +0530

    Added sound for score

--- components/score.js
+++ components/score.js
@@ -1,9 +1,12 @@
 import { EXPANSION_RATE } from "./food.js";
 
+var sound = new Audio("../assets/chime.mp3");
+
 let score = 0;
 
 export function update() {
     score += EXPANSION_RATE;
+    sound.play();
 }
 
 export function draw(scoreBoard) {
